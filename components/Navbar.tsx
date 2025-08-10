c14d53db7b44dd73a1c082686e991d1934e9d705
Caliver <caliver@caliverlabs.com>
2025-08-10 14:02:20 -0500

    feat: added HeroUI

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
+import Link from "next/link";
+
+export function NavbarComponent() {
+  return (
+    <Navbar shouldHideOnScroll isBordered>
+      <NavbarBrand>
+        <Link href="/">
+          <p className="text-2xl font-bold">Caliver</p>
+        </Link>
+      </NavbarBrand>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
+        <NavbarItem>
+          <Link href="/about">About</Link> 
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/work">Work</Link> 
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/blog">Blog</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/contact">Contact</Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
