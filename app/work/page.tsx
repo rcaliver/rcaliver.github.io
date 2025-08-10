8246d412e5b4f80c90307c276b2c981ad1230af4
Caliver <caliver@caliverlabs.com>
2025-08-10 16:32:17 -0500

    refactor: removido CardFooter da página Work para simplificação do componente

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,9 +6,11 @@ import { PageTitle } from "@/components/PageTitle"
 import { Card, CardHeader } from "@heroui/card";
 import { Image } from "@heroui/image";
 import { Chip } from "@heroui/chip";
+import Link from "next/link";
 
-const projects = [
+export const projects = [
     {
+        id: "showup1-web",
         title: "SHOWUP1 - Web",
         type: "web",
         client: "SHOWUP1",
@@ -18,6 +20,7 @@ const projects = [
         tech: ["Laravel 12", "Next.js", "Tailwind CSS", "MySQL", "Redis", "Docker", "AWS", "CI/CD", "Grafana", "Prometheus", "OpenTelemetry"],
     },
     {
+        id: "showup1-mobile",
         title: "SHOWUP1 - Mobile",
         type: "mobile",
         client: "SHOWUP1",
@@ -27,6 +30,7 @@ const projects = [
         tech: ["React Native", "TypeScript", "Expo", "Firebase", "AWS", "CI/CD", "Grafana", "Prometheus", "OpenTelemetry"],
     },
     {
+        id: "ai-integration",
         title: "AI Integration",
         type: "web",
         client: "Agency Analytics",
@@ -36,6 +40,7 @@ const projects = [
         tech: ["Laravel 12", "React", "MySQL", "Redis", "GCP", "Grafana", "Prometheus", "OpenTelemetry"],
     },
     {
+        id: "database-optimization",
         title: "Database Optimization",
         type: "server",
         client: "Agency Analytics",
@@ -54,7 +59,7 @@ export default function Work() {
 
         <div className="grid mt-13 grid-cols-2 gap-4">
             {projects.map((project) => (
-                <Card key={project.title} className="rounded-lg h-[300px] overflow-hidden">
+                <Card as={Link} href={`/work/${project.id}`} key={project.id} className="rounded-lg h-[300px] overflow-hidden">
                     <CardHeader className="absolute z-10 top-1 flex-col items-start!">
                         <p className="text-white/80 uppercase font-bold text-sm">{project.title}</p>
                         <div className="flex gap-2 mt-1">
@@ -65,7 +70,7 @@ export default function Work() {
                     </CardHeader>
                     <Image
                         removeWrapper
-                        alt={project.title}
+                        alt={project.id}
                         className="z-0 w-full h-full object-cover rounded-lg"
                             src={project.image}
                         />
