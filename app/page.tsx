837628762f0a23c5c20a1079bd4f0b920342f8aa
Caliver <caliver@caliverlabs.com>
2025-08-10 14:02:37 -0500

    feat: added Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Hi! I&apos;m Caliver</h1>
-      </main>
+    <div className="max-w-4xl mx-auto mt-36">
+      <h1 className="text-4xl font-sans text-center font-bold text-6xl/13 uppercase">
+        Hey, I'm Rodrigo Silva but you can call me <span className="text-red-500">Caliver</span>!
+      </h1>
+      <div className="max-w-md mx-auto">
+        <p className="mt-13 text-lg text-center font-mono">I'm a full stack developer with 12+ years of experience and an aspiring software architect</p>
+        <div className="mt-13 flex justify-between gap-4 text-center">
+          <div className="flex items-center gap-2">
+            <span className="text-lg font-mono">→</span>
+            <Link href="/projects" className="text-lg font-mono">see my projects</Link>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-lg font-mono">→</span>
+            <Link href="/contact" className="text-lg font-mono">contact me</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
