f61ef55eb714a3b8d16b3a14a844c91f792732da
Caliver <caliver@caliverlabs.com>
2025-08-10 14:10:24 -0500

    fix: corrigido uso de apostrofo em strings na página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className="max-w-4xl mx-auto mt-36">
       <h1 className="text-4xl font-sans text-center font-bold text-6xl/13 uppercase">
-        Hey, I'm Rodrigo Silva but you can call me <span className="text-red-500">Caliver</span>!
+        Hey, I&apos;m Rodrigo Silva but you can call me <span className="text-red-500">Caliver</span>!
       </h1>
       <div className="max-w-md mx-auto">
-        <p className="mt-13 text-lg text-center font-mono">I'm a full stack developer with 12+ years of experience and an aspiring software architect</p>
+        <p className="mt-13 text-lg text-center font-mono">I&apos;m a full stack developer with 12+ years of experience and an aspiring software architect</p>
         <div className="mt-13 flex justify-between gap-4 text-center">
           <div className="flex items-center gap-2">
             <span className="text-lg font-mono">→</span>
